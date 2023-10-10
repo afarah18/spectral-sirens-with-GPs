323e1000faf0f7f226329f3b7eda7b6a3c3b229d
Amanda Farah <amanda.farah@ligo.org>
2023-10-10 10:49:47 -0500

    add GWMockCat and test arviz

--- src/scripts/nonparametric_plots.py
+++ src/scripts/nonparametric_plots.py
@@ -1,12 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
-# import arviz as az
+import arviz as az
 
 import paths
 
 # load in inference results
-# mcmc = az.InferenceData.from_netcdf(paths.data / "mcmc_nonparametric.nc4")
-# samples = mcmc.posterior
+mcmc = az.InferenceData.from_netcdf(paths.data / "mcmc_nonparametric.nc4")
+samples = mcmc.posterior
+# plot them
+
 
 # plot true underlying disribution
 # import it from gwpop
