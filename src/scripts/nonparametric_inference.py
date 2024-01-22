607c3f2ccad95ab26b853ccddfec6dcd53034a31
Amanda Farah <amanda.farah@ligo.org>
2024-01-21 22:28:09 -0600

    fewer samples to make testing faster

--- src/scripts/nonparametric_inference.py
+++ src/scripts/nonparametric_inference.py
@@ -108,7 +108,7 @@ if  __name__ == "__main__":
     kwargs = dict(m1det=m1z_PE,dL=dL_PE, m1det_inj=m1zinj_det,dL_inj=dLinj_det,
                     log_pinj=log_pinj_det, log_PE_prior=log_PE_prior,
                     remove_low_Neff=remove_low_Neff)
-    mcmc = numpyro.infer.MCMC(nuts_kernel,num_warmup=100,num_samples=200,
+    mcmc = numpyro.infer.MCMC(nuts_kernel,num_warmup=10,num_samples=20,
                               num_chains=1,progress_bar=True)   
     mcmc.run(jax_rng,**kwargs)
 
