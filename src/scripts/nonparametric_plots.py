79745acc30115af2e62e789bb1647b006aa15959
Amanda Farah <amanda.farah@ligo.org>
2024-01-21 22:10:09 -0600

    add true distribution line, clean up code

--- src/scripts/nonparametric_plots.py
+++ src/scripts/nonparametric_plots.py
@@ -1,39 +1,45 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import arviz as az
+from scipy.stats import gaussian_kde
 
 from nonparametric_inference import TEST_M1S
 from data_generation import H0_FID
+from gwpop import powerlaw_peak
+
 import paths
 
 plt.style.use(paths.scripts / "matplotlibrc")
 
-# load in inference results
-mcmc = az.InferenceData.from_netcdf(paths.data / "mcmc_nonparametric.nc4")
-samples = mcmc.posterior
-r = np.exp(samples['log_rate'][0])
-rt = np.exp(samples['log_rate_test'][0])
+def two_panel(path):
+    id = az.InferenceData.from_netcdf(path)
+    samples = id.posterior
+    try:
+        r = np.nan_to_num(np.exp(samples['log_rate_test'][0]))
+    except KeyError :
+        r = np.nan_to_num(np.exp(samples['log_rate'][0]))
 
-# plot them
-fig, axes = plt.subplots(ncols=2,figsize=(6.5,3))
-for i in range(200):
-    gp_norm = np.trapz(rt[i],TEST_M1S)
-    axes[0].plot(TEST_M1S, rt[i]/gp_norm, lw=0.2, c='b',alpha=0.1)
+    fig, axes = plt.subplots(ncols=2,figsize=(20,8),facecolor='none',gridspec_kw={'width_ratios': [1.2, 1]})
+    for i in range(200):
+        PLP_norm = np.trapz(r[i],TEST_M1S)*1.15
+        axes[0].plot(TEST_M1S, r[i]/PLP_norm, lw=0.2, c="blue",alpha=0.1)
+        
+    axes[0].plot(TEST_M1S, 7*powerlaw_peak(TEST_M1S,alpha=-2.7,f_peak=0.05,mMax=78.0,mMin=10.0,mu_m1=30.0,sig_m1=7.0),
+                c='k',lw=3,label='Underlying distribution')
+    axes[0].set_yscale('log')
+    axes[0].set_ylim(1e-5,1e-1)
+    axes[0].set_xlim(1,80)
 
-axes[0].plot([],linewidth=0.5,alpha=0.5, label='Gaussian process fit')
+    prior=np.linspace(55,120,num=200)
+    ho_kde=gaussian_kde(samples['H0'])
+    axes[1].plot(prior,ho_kde(prior))
 
-# plot true underlying disribution
-# TODO: import it from gwpop
-m1s_true = np.load(paths.data / "gw_data/m1s_true_feature_full.npy")
-axes[0].hist(m1s_true,bins=50,histtype='step',color='black',label = 'underlying data',density=True)
+    axes[1].axvline(67.66,color='k',ls='--',label="True value")
+    axes[1].set_xlabel('$H_0$ [km/s/Mpc]')
+    axes[1].set_ylabel('posterior density')
+    axes[1].legend(framealpha=0)
+    plt.tight_layout()
+    fig.savefig(paths.figures / "O5_GP.pdf")
+    plt.clf()
 
-axes[0].set_xlabel("$m_{1, \\mathrm{source}}$ [$M_{\\odot}$]")
-axes[0].legend()
-axes[0].set_xlim(right=110,left=0)
-
-# H0 posterior
-az.plot_density(mcmc,var_names='H0',ax=axes[1])
-axes[1].axvline(H0_FID,color='k')
-axes[1].set_xlabel('$H_0$ [km/s/Mpc]')
-axes[1].set_ylabel('posterior_density')
-fig.savefig(paths.figures / "O5_GP.pdf")
+two_panel(paths.data / "mcmc_nonparametric.nc4")
