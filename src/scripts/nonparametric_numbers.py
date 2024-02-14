4a7f829d59ed9bc37933ec34a95f1f8e3f22b41a
Amanda Farah <amanda.farah@ligo.org>
2024-02-14 17:37:52 -0600

    formatting and algebra fix

--- src/scripts/nonparametric_numbers.py
+++ src/scripts/nonparametric_numbers.py
@@ -4,9 +4,10 @@ import paths
 
 id = az.InferenceData.from_netcdf(paths.data / "mcmc_nonparametric.nc4")
 h0samps = id.posterior['H0'][0]
-print(len(h0samps))
 
 with open(paths.output / "nonparh0percent.txt", "w") as f:
     print(f"{np.std(h0samps)/np.mean(h0samps):.1f}", file=f)
+lower = np.mean(h0samps)-np.percentile(h0samps,5)
+upper = np.percentile(h0samps,95)-np.mean(h0samps)
 with open(paths.output / "nonparh0CI.txt", "w") as f:
-    print(f"${np.mean(h0samps):.1f}"+"^{+"+f"{np.percentile(h0samps,95)}"+"}"+"_{-"+f"{np.percentile(h0samps,95)}"+"}$", file=f)
+    print(f"${np.mean(h0samps):.1f}"+"^{+"+f"{lower:.1f}"+"}"+"_{-"+f"{upper:.1f}"+"}$", file=f)
