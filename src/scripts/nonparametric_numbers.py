f8de214ac86684b14898b6348b64c74e54ba0e1f
showyourwork <showyourwork>
2024-02-14 17:38:04 -0600

    [showyourwork] overleaf sync

--- src/scripts/nonparametric_numbers.py
+++ src/scripts/nonparametric_numbers.py
@@ -6,7 +6,7 @@ id = az.InferenceData.from_netcdf(paths.data / "mcmc_nonparametric.nc4")
 h0samps = id.posterior['H0'][0]
 
 with open(paths.output / "nonparh0percent.txt", "w") as f:
-    print(f"{np.std(h0samps)/np.mean(h0samps):.1f}", file=f)
+    print(f"{np.std(h0samps)/np.mean(h0samps)*100:.1f}", file=f)
 lower = np.mean(h0samps)-np.percentile(h0samps,5)
 upper = np.percentile(h0samps,95)-np.mean(h0samps)
 with open(paths.output / "nonparh0CI.txt", "w") as f:
