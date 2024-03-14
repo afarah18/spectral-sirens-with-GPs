39a1b91d24d29ef10fc3ec2a5457d18d8de9d806
Amanda Mirna Farah <afarah@uchicago.edu>
2024-03-14 12:09:14 -0500

    update

--- src/scripts/submit.sh
+++ src/scripts/submit.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=paper_parametric
-#SBATCH --output=/home/afarah/p2/spectral-sirens-with-GPs/src/outfiles/paper_parametric.out
-#SBATCH --error=/home/afarah/p2/spectral-sirens-with-GPs/src/outfiles/paper_parametric.err
+#SBATCH --job-name=paper_fitOm0_gwmc
+#SBATCH --output=/home/afarah/p2/spectral-sirens-with-GPs/src/outfiles/paper_fitOm0_gwmc.out
+#SBATCH --error=/home/afarah/p2/spectral-sirens-with-GPs/src/outfiles/paper_fitOm0_gwmc.err
 #SBATCH --account=kicp
 #SBATCH --time=48:00:00
-#SBATCH --partition=kicp
+#SBATCH --partition=kicp-gpu
 #SBATCH --nodes=1
 #SBATCH --mem=128G
+#SBATCH --gres=gpu:1
 
 module load cuda/11.5
 echo "loaded cuda"
@@ -14,4 +15,4 @@ module load python/anaconda-2022.05
 echo "loaded anaconda"
 source activate jax_gpu
 echo "activated env"
-python /home/afarah/p2/spectral-sirens-with-GPs/src/scripts/parametric_inference.py
+python /home/afarah/p2/spectral-sirens-with-GPs/src/scripts/nonparametric_inference_fitOm0.py
