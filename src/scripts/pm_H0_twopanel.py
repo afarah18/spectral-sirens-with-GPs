some fixes to tide us over until new runs are done (incl normalizing mass dist)

src/scripts/pm_H0_twopanel.py
@@ -5,7 +5,7 @@ from scipy.stats import gaussian_kde
 
 from priors import TEST_M1S
 from nonparametric_inference import NSAMPS
-from data_generation import H0_FID
+from data_generation import H0_FID,TRUEVALS
 from gwpop import powerlaw_peak
 
 import paths
@@ -18,24 +18,31 @@ def two_panel(path, path_PLP, path_BPL, hyperparam='H0'):
     r = np.nan_to_num(np.exp(samples['log_rate_test'][0]))
 
     id_PLP = az.InferenceData.from_netcdf(path_PLP)
-    samples_PLP = id_PLP.posterior
-    r_PLP = np.nan_to_num(np.exp(samples_PLP['log_rate'][0]))
+    samples_PLP = id_PLP.posterior.sel(chain=0).reset_coords("chain",drop=True)
+    r_PLP = np.nan_to_num(np.exp(samples_PLP['log_rate']))
     
     id_BPL = az.InferenceData.from_netcdf(path_BPL)
-    samples_BPL = id_BPL.posterior
-    r_BPL = np.nan_to_num(np.exp(samples_BPL['log_rate'][0]))
+    samples_BPL = id_BPL.posterior.sel(chain=0).reset_coords("chain",drop=True)
+    r_BPL = np.nan_to_num(np.exp(samples_BPL['log_rate']))
+    m1s_plotting_parametric = np.linspace(0,TEST_M1S.max(),len(TEST_M1S)) # TODO: remove this w/ new runs
 
     fig, axes = plt.subplots(ncols=2,figsize=(7.5,4*.75),facecolor='none',gridspec_kw={'width_ratios': [1.2, 1]})
     for i in range(NSAMPS//2):
-        axes[0].plot(TEST_M1S, r[i], lw=0.1, c="blue",alpha=0.03)
-        axes[0].plot(TEST_M1S, r_PLP[i],lw=0.1, c="green",alpha=0.03)
-        axes[0].plot(TEST_M1S, r_BPL[i],lw=0.1, c="orange",alpha=0.03)
+        axes[0].plot(TEST_M1S, r[i]/np.trapz(y=r[i],x=TEST_M1S), lw=0.1, c="blue",alpha=0.03)
+        # TODO: get rid of indexing and plot parametric with TEST_M1S when new parametric runs are done with proper TEST_M1S
+        # TODO: get rid of normalization when new parametric and nonparametric runs are done
+        axes[0].plot(m1s_plotting_parametric, r_PLP[i]/np.trapz(y=r_PLP[i][1:],x=TEST_M1S[1:]),lw=0.1, c="green",alpha=0.03)
+        axes[0].plot(m1s_plotting_parametric, r_BPL[i]/np.trapz(y=r_BPL[i][1:],x=TEST_M1S[1:]),lw=0.1, c="orange",alpha=0.03)
     
-    axes[0].plot(TEST_M1S, samples_PLP['rate'].mean(axis=1)*powerlaw_peak(TEST_M1S,alpha=-2.7,f_peak=0.05,mMax=78.0,mMin=10.0,mu_m1=30.0,sig_m1=7.0),
+    axes[0].plot(TEST_M1S, powerlaw_peak(TEST_M1S,alpha=TRUEVALS['alpha'],f_peak=TRUEVALS['f_peak'],mMax=TRUEVALS['mmax'],
+                                         mMin=TRUEVALS['mmin'],mu_m1=TRUEVALS['mu_m1'],sig_m1=TRUEVALS['sig_m1']
+                                         )/np.trapz(y= powerlaw_peak(TEST_M1S,alpha=TRUEVALS['alpha'],f_peak=TRUEVALS['f_peak'],mMax=TRUEVALS['mmax'],
+                                         mMin=TRUEVALS['mmin'],mu_m1=TRUEVALS['mu_m1'],sig_m1=TRUEVALS['sig_m1']),x=TEST_M1S),
                 c='k')
     axes[0].set_yscale('log')
     axes[0].set_xscale('log')
-    axes[0].set_ylim(5e-4,10)
+    # axes[0].set_ylim(5e-4,10) # TODO: uncomment this and comment out below when we get rid of normalization
+    axes[0].set_ylim(5e-6,.1)
     axes[0].set_xlim(5.,100)
     axes[0].set_xlabel("$m_1 \,$[M$_{\odot}$]")
     axes[0].set_ylabel(r"$\frac{d N}{d m_1}\,$[M$_{\odot}^{-1}$Gpc$^{-3}$yr$^{-1}$]")
@@ -59,8 +66,8 @@ def two_panel(path, path_PLP, path_BPL, hyperparam='H0'):
     axes[1].plot(prior,kde_PLP(prior),c='green',label=r'\textsc{Power Law + Peak}')
     axes[1].plot(prior,kde_BPL(prior),c='orange',label='Broken power law')
     axes[1].set_ylabel('posterior density')
-    fig.legend(ncol=4,framealpha=0,fontsize=9,loc="upper center")
-    plt.tight_layout()
+    fig.legend(ncol=4,framealpha=0,loc="outside upper center")
+    # plt.tight_layout()
     fig.savefig(paths.figures / "O5_pm.pdf")
     plt.clf()
 