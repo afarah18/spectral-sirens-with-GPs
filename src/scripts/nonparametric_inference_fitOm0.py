0213d59fba6a574f1b5721a522eca6ab0ad25937
Amanda Mirna Farah <afarah@uchicago.edu>
2024-06-28 05:09:13 -0500

    add m2 fitting

--- src/scripts/nonparametric_inference_fitOm0.py
+++ src/scripts/nonparametric_inference_fitOm0.py
@@ -11,7 +11,7 @@ import numpyro
 # data
 import numpy as np
 
-NSAMPS = 5
+NSAMPS = 50
 
 jax.config.update("jax_enable_x64", True)
 # random number generators
@@ -22,6 +22,7 @@ jax_rng = jax.random.PRNGKey(42)
 if  __name__ == "__main__":
     # load data
     m1z_PE = np.load(paths.data / "gw_data/m1z_PE.npy")
+    m2z_PE = np.load(paths.data / "gw_data/m2z_PE.npy")
     dL_PE = np.load(paths.data / "gw_data/dL_PE.npy")
     log_PE_prior = np.load(paths.data / "gw_data/log_PE_prior.npy")
 
@@ -37,7 +38,7 @@ if  __name__ == "__main__":
     # Inference
     nuts_settings = dict(target_accept_prob=0.9, max_tree_depth=10,dense_mass=False)
     nuts_kernel = numpyro.infer.NUTS(hyper_prior,**nuts_settings)
-    kwargs = dict(m1det=m1z_PE,dL=dL_PE, m1det_inj=m1zinj_det,dL_inj=dLinj_det,
+    kwargs = dict(m1det=m1z_PE,dL=dL_PE, m2det=m2z_PE, m1det_inj=m1zinj_det,dL_inj=dLinj_det,
                     log_pinj=log_pinj_det, log_PE_prior=log_PE_prior,
                     PC_params=dict(conc=conc,concentration=concentration,scale=scale,lam_sigma=lam_sigma),
                     remove_low_Neff=remove_low_Neff,fit_Om0=True)
